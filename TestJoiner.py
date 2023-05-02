ac10fb14a2bdf9087dc16620b5ce4c4b4b4a6dcc
Dustin Hurtz <dhurtz@nevada.unr.edu>
2023-05-02 12:23:43 -0700

    commenting

--- TestJoiner.py
+++ TestJoiner.py
@@ -2,6 +2,6 @@ import unittest
 from Joiner import Joiner
 
 class TestJoiner(unittest.TestCase):
-    def test_outerJoin(self):
+    # def test_outerJoin(self):
 
-    def test_innerJoin(self):
+    # def test_innerJoin(self):
--- TestModifyCSV.py
+++ TestModifyCSV.py
@@ -2,18 +2,18 @@ import unittest
 from ModifyCSV import ModifyCSV
 
 class TestModifyCSV(unittest.TestCase):
-    def test_writeHeaders(self):
+    # def test_writeHeaders(self):
 
-    def test_insertValues(self):
+    # def test_insertValues(self):
     
-    def test_readSpecificValuesNotEqual(self):
+    # def test_readSpecificValuesNotEqual(self):
     
-    def test_changeValueWithSameColumn(self):
+    # def test_changeValueWithSameColumn(self):
 
-    def test_changeValueWithDifferentColumn(self):
+    # def test_changeValueWithDifferentColumn(self):
     
-    def test_deleteValuesEqual(self):
+    # def test_deleteValuesEqual(self):
     
-    def test_deleteValuesGreater(self):
+    # def test_deleteValuesGreater(self):
 
-    def test_removeOuterParentheses(self):
+    # def test_removeOuterParentheses(self):
